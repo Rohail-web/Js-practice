5ec88efc3a86e88322a2015480fe3f1da3c79262
Rohail-web <irohail639@gmail.com>
2024-12-10 08:21:30 +0000

    strings and its types

---
+++ 01_basics/04_string.js
@@ -0,0 +1,51 @@
+const name = "rohail"
+const age = 18
+console.log(`hi my name is ${name} and my age is ${age}`);
+
+const gamename = new String('Counter Strike')
+console.log(gamename.length);// counting the lenght of string
+
+console.log(gamename.toUpperCase());// for making string in Capital
+
+console.log(gamename[0]);// Calling a string with its index value
+
+console.log(gamename.charAt(8));// use for finding a word in string
+
+console.log(gamename.indexOf('S'));// use for finding the index value of string
+
+console.log(gamename.trim());//use for escaping extra spaces and characters
+
+console.log(gamename.substring(0,7));// use for printing a string by giving start and end
+
+const anothergame = gamename.slice(-7,-1)
+console.log(anothergame);
+const url = "http://youtube.com"
+console.log(url.replace('youtube','facebook'));//use for relplacing words in strings
+
+
+ 
+// for (let i = 1; i<=5; ++i) {
+//     console.log(i);
+    
+// }
+
+// var i = 42;
+// console.log(i++); // shows 42
+// console.log(i); // shows 43
+// i = 42;
+// console.log(++i); // shows 43
+// console.log(i); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
