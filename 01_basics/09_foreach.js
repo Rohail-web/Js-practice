fb9feeafdbfedb1e7207b9957d3514b94f2715cb
Rohail-web <irohail639@gmail.com>
2024-12-17 11:12:37 +0000

    foreach

---
+++ 01_basics/09_foreach.js
@@ -0,0 +1,18 @@
+const myarr = [
+    {
+        name : 'ROHAIL',
+        Lastname : 'ali'
+    },
+    {
+        name : 'abdullah',
+        Lastname : 'abdul'
+    },
+    {
+        name : 'samad',
+        Lastname : 'khan'
+    }
+]
+myarr.forEach((item)=>{
+    console.log(item.name);
+    
+})
