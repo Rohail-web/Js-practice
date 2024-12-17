5518c138a1e4b139e6c499e8578514800f029fe2
Rohail-web <irohail639@gmail.com>
2024-12-17 11:12:22 +0000

    loops

---
+++ 08_loops.js
@@ -0,0 +1,55 @@
+// for (let index = 0; index <= 10; index = index +2) {
+//     const element = index;
+//     console.log(element);
+// }
+// let index = 0
+// while (index < 20) {
+//     index++; 
+//     console.log(index);
+//     if (index == 19) {
+//         break;
+//     }
+//     if (index == 10) {
+//         console.log("number 10 detected");
+//         continue;
+//     }
+//    }
+//  let index = 20
+//  while (index > 0) {
+//     index--;
+//     console.log(index);
+    
+//  }
+                // multiplication with loops
+// let index = 7;
+// console.log(`The multiplication of ${index}: `);
+// for (let i = 0; i <= 10; i++) {
+//    console.log(`${index} x ${i} = ${index*i}`);
+// }
+
+// let number = 8;
+// let i = 1;
+// while (i<=10) {
+//     console.log(`${number} x ${i} = ${number * i}`);
+//     i++;
+// }
+
+                    // calculate factorial through loops
+
+    // let n = 5
+    // let factorial = 1
+    // for (let i = 1; i <= n  ; i++) {
+    //     factorial*=i
+    // }
+    // console.log(`The factorial of ${n} is ${factorial}`);
+    
+    
+
+    // let number = 5;
+    // for (let i = 1; i <= 10; i++) {
+    //         console.log(`${number} x ${i} = ${number*i}`); 
+    // }
+
+
+
+
