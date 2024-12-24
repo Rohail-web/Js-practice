02b90c8042459304339036cf832c35850634d3b5
Rohail-web <irohail639@gmail.com>
2024-12-24 06:39:39 +0000

    apis

--- 01_basics/13_api.js
+++ 01_basics/13_api.js
@@ -1,23 +1,23 @@
 const fetchdata = async () => {
     try {
-        const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts");
         if (!response.ok) {
-            throw new Error(`HTTP error: status ${response}`);
+            throw new Error("ERROR");
         }
-            const data = await response.json()  
-            const obj = {
-                title:[
-                    ...data.map((item)=>(item.title))
-                ]
-            };
-            console.log(obj);
-            
-           
+        const data = await response.json();              // {[]}
+        // const obj = {
+        //     fetch_data: data.map((item) => ({
+        //         titles: item.title,
+        //         userID: item.userId,
+        //         id: item.id,
+        //         body: item.body
+        //     })),
+        // };
+        const obj =  data.map((item)=>({titles :item.title,id :item.id}))         // [{}]
+        console.log(obj);
 
-        
     } catch (error) {
-        console.error(error);
-        
+        console.error(message, error);
     }
 }
 fetchdata();
